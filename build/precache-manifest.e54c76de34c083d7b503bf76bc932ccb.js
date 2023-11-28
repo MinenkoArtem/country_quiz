e96fc08d050169ae32fa4d4358f25454c8de3629
MinenkoArtem <148456343+MinenkoArtem@users.noreply.github.com>
2023-11-28 11:14:43 +0200

    Add files via upload

---
+++ build/precache-manifest.e54c76de34c083d7b503bf76bc932ccb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4bc531bfae9fcf001b0",
+    "url": "/static/css/main.842eaf65.chunk.css"
+  },
+  {
+    "revision": "f4bc531bfae9fcf001b0",
+    "url": "/static/js/main.efd7ea73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "570c5deaf302a794a30e",
+    "url": "/static/js/2.13062d56.chunk.js"
+  },
+  {
+    "revision": "813ffc91637bebb9539ef7d3ad2d4741",
+    "url": "/static/media/click.813ffc91.mp3"
+  },
+  {
+    "revision": "70df21b9af0f1b05f24b92e5ffb6f044",
+    "url": "/static/media/score.70df21b9.mp3"
+  },
+  {
+    "revision": "4c8e826ed8c507ed00873d0db6395aee",
+    "url": "/static/media/bg.4c8e826e.jpg"
+  },
+  {
+    "revision": "c8708fb13e622588a503737a1baf8824",
+    "url": "/static/media/Ubuntu-Bold.c8708fb1.woff2"
+  },
+  {
+    "revision": "bdd8b7a495f5ebf07470f66324eab07d",
+    "url": "/static/media/Ubuntu-Medium.bdd8b7a4.woff2"
+  },
+  {
+    "revision": "3303751b2be70b201dc8b035a0e15751",
+    "url": "/static/media/Ubuntu-Regular.3303751b.woff2"
+  },
+  {
+    "revision": "fffa35e88ed8a2a66286484cae881691",
+    "url": "/index.html"
+  }
+];
